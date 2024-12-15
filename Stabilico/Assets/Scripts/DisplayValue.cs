e052f07ae00b8bb1a97a8d4e28ff27c0b227c75b
Sightyyy <medtr333@gmail.com>
2024-12-16 02:26:49 +0700

    adding more ui

---
+++ Stabilico/Assets/Scripts/DisplayValue.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DisplayValue : MonoBehaviour
+{
+    private GameData gameData;
+    public TextMeshProUGUI Budget;
+    public TextMeshProUGUI Worker;
+    public TextMeshProUGUI Happiness;
+    public TextMeshProUGUI Trust;
+
+    void Start()
+    {
+        gameData = GameData.instance; // Mengambil instance GameData
+    }
+
+    void Update()
+    {
+        // Memperbarui nilai setiap kali frame
+        if (gameData != null)
+        {
+            Budget.text = gameData.playerFinance.ToString();
+            Worker.text = gameData.workerAmount.ToString();
+            Happiness.text = gameData.workerHappiness.ToString(); // Menggunakan format desimal untuk 2 angka di belakang koma
+            Trust.text = gameData.clientTrust.ToString(); // Menggunakan format desimal untuk 2 angka di belakang koma
+        }
+    }
+}
